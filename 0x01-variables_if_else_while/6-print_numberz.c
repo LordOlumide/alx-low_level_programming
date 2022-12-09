ff365fb80d68f289b516c17e823890cf46da6e03
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-09 15:14:00 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Numberzz'
+ *
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+
+	return (0);
+}
