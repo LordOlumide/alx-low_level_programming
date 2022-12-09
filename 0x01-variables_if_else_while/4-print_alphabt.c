177f43c440c79cc4c2a29dc2569b20648d83f268
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-09 15:01:47 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Print Exclude'
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		if (n != 101 && n != 113)
+			putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
