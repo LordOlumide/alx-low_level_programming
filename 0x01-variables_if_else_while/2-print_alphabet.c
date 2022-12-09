48afe3b035e7bbc5f5b51d2f371b579dc7713fea
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-09 14:39:53 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Hot to odkj'
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 97; a <= 122; a++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
