e1c97545fd14db9e4b56607b9d22541926f8af65
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-09 14:23:47 -0800

    zero

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,8 +22,8 @@ int main(void)
 	if (last_digit > 5)
 		printf("and is greater than 5\n");
 	else if (last_digit == 0)
-		printf("and is zero");
+		printf("and is 0");
 	else
-		printf("and is less than 6 and not zero\n");
+		printf("and is less than 6 and not 0\n");
 	return (0);
 }
