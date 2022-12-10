c5676531f5900b1c58b3a117b06ffc87553f18ae
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-09 16:34:16 -0800

    jsdn

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Two number combinations'
+ *
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int a;
+	int i;
+
+	for (a = 48; a <= 56; a++)
+	{
+		for (i = a + 1; i <= 57; i++)
+		{
+			putchar(a);
+			putchar(i);
+			if (a != 56)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
