0dddee26047301877ef23b9a27f70e927daf6354
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-13 14:48:18 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'Prints the alphabets'
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,24 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-void _putchar(void)
+/**
+ * print_alphabet - void function
+ *
+ * Description: 'Prints all letters'
+ *
+ * Return: ALways void(Success)
+ */
+
+void print_alphabet(void)
 {
-	return;
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
 }
+
+#endif
