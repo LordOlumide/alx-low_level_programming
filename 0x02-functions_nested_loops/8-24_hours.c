ad0ab71060b05285b45c9dd1d144717b48aeea94
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-14 02:41:45 -0800

    dsnov

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,31 +8,36 @@
 
 void jack_bauer(void)
 {
-	int a = 0, b = 0, c = 0, d = 0;
+	int a = 0, first = 0, second = 0, c = 0, d = 0;
 
-	while (a <= 2)
+	while (a <= 23)
 	{
-		while (b <= 3)
+		while (c <= 5)
 		{
-			while (c <= 5)
+			while (d <= 9)
 			{
-				while (d <= 9)
+				if (a < 10)
 				{
-					_putchar(a + '0');
-					_putchar(b + '0');
-					_putchar(':');
-					_putchar(c + '0');
-					_putchar(d + '0');
-					_putchar('\n');
-					d++;
+					first = 0;
+					second = a;
 				}
-				d = 0;
-				c++;
+				else if (a > 20)
+				{
+					first = a / 10;
+					second = a % 10;
+				}
+				_putchar(first + '0');
+				_putchar(second + '0');
+				_putchar(':');
+				_putchar(c + '0');
+				_putchar(d + '0');
+				_putchar('\n');
+				d++;
 			}
-			c = 0;
-			b++;
+			d = 0;
+			c++;
 		}
-		b = 0;
+		c = 0;
 		a++;
 	}
 }
