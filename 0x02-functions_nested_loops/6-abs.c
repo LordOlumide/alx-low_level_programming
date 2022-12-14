9fe67bb2e09bc099f2c1b14eec2771ebe6750328
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-14 01:42:20 -0800

    int _abs(int);

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - returns the absolute value of an integer
+ * @c: value to be returned
+ *
+ * Return: Absolute vale of input
+ */
+
+int _abs(int c)
+{
+	if (c >= 0)
+	{
+		return (c);
+	}
+	return (-c);
+}
