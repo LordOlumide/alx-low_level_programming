ca9a8b799e7b664df7dd9fb7b464e77a4c083b5c
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-14 03:03:10 -0800

    dinvirjns

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,15 +8,19 @@
 
 void times_table(void)
 {
-	int a = 0, b = 0, c = 0, d = 0;
-	int e = 0, f = 0, g = 0, h = 0, i = 0;
-	int no_of_times = 0, adder = 0, adder_incrementer = 0;
+	int a = 0;
+	int no_of_times = 0, adder = 0;
 
 	while (no_of_times < 9)
 	{
 		while (adder < 9)
 		{
 			_putchar(a + '0');
+			if (adder != 8)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 			a += adder;
 			adder++;
 		}
