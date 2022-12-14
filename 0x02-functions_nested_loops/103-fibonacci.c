4a0598582a6252852fa4da2d2bc325ca55fb28e1
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-14 06:50:31 -0800

    sdn

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: 'fib'
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c, i, total;
+	
+	a = 1;
+	b = 2;
+	total = 0;
+
+	total += 2;
+
+	for (i = 0; i < 34; i++)
+	{
+		c = a + b;
+		if (c % 2 == 0)
+		{
+			total += c;
+		}
+		a = b;
+		b = c;
+	}
+	printf("%d", total);
+}
