e872133af4f7adc45f6565b46ac176544e58bde7
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-14 06:14:42 -0800

    den

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * natural - print sum of multiples of 3 or 5
+ *
+ * Return: Always void
+ */
+
+void natural(void)
+{
+	int max = 1024;
+	int n = 0;
+	int total = 0;
+
+	while (n < max)
+	{
+		if ((n % 3 == 0) || (n % 5) == 0)
+			total += n;
+		n++;
+	}
+	printf("%d\n", &total);
+}
