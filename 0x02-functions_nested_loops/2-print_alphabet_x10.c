5dd94e1615aa5c785a68b2d5eb26c61559f57a3f
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-14 00:57:24 -0800

    dsjsf

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints alphabets 10 times
+ *
+ * Description: 'Prints 10 times'
+ *
+ * Return: Always void(Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int n = 97;
+	while (i < 10)
+	{
+		while (n <= 122)
+		{
+			_putchar(n);
+		}
+		_putchar('\n');
+	}
+}
