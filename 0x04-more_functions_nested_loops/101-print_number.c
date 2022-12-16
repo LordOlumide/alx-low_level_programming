b47befeee0c7f155583731bfeb5277861d152476
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-15 19:46:33 -0800

    sdsd

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -14,34 +14,9 @@ void print_number(int n)
 		n = -n;
 	}
 
-	if  (n < 10)
+	if  (n > 9)
 	{
-		_putchar(n + '0');
+		print_number(n / 10);
 	}
-	else if (n < 100)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n < 1000)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n % 100) / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n < 10000)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n % 1000) / 100) + '0');
-		_putchar((((n % 1000) % 100) / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n < 100000)
-	{
-		_putchar((n / 10000) + '0');
-		_putchar(((n % 10000) / 1000) + '0');
-		_putchar((((n % 10000) % 1000) / 100) + '0');
-		_putchar(((((n % 10000) % 1000) % 100) / 10) + '0');
-		_putchar((n % 10) + '0');
-	}
+	_putchar((n % 10) + '0');
 }
