cbd413f13495b4b07710d37fe5fc162622e67e87
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-15 16:20:43 -0800

    dsj

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints 0 to 9 minus 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		if (a != 2 && a != 4)
+		{
+			_putchar(a + '0');
+		}
+	}
+}
