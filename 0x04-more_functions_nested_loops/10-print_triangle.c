121fe712163fc7198d95e333f7b4732929736363
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-15 18:18:05 -0800

    w

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle
+ * @size: size of the triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int width, a, b;
+
+	for (width = 1; width <= size; width++)
+	{
+		a = size - width;
+		while (a > 0)
+		{
+			_putchar(32);
+			a--;
+		}
+		for (b = width; b > 0; b--)
+		{
+			_putchar(35);
+		}
+		_putchar(10);
+	}
+}
