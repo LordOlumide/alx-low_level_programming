2bce772fb08ffc176000e0e67001296bc0bf8f0c
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-15 19:16:44 -0800

    a

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number
+ * @c: number to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	
+	if  (n < 10)
+	{
+		_putchar(n + '0');
+	}
+	else if (n < 100)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 1000)
+	{
+		_putchar((n / 100) + '0');
+		_putchar(((n % 100) / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 10000)
+	{
+		_putchar((n / 1000) + '0');
+		_putchar(((n % 1000) / 100) + '0');
+		_putchar((((n % 1000) % 100) / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+}
