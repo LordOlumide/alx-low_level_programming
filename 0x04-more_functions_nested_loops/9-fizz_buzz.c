43834a56bf6d0f6f58119cbcc503d90a0b0055bc
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-15 17:31:37 -0800

    sd

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: FizzBuzz
+ * Return: 0(Success)
+ */
+
+int main(void)
+{
+	int min, max;
+
+	max = 100;
+
+	for (min = 1; min <= 100; min++)
+	{
+		if ((min % 3 == 0) && (min % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (min % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (min % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+			_putchar( min + '0');
+
+		_putchar(32);
+	}
+	_putchar(10);
+}
