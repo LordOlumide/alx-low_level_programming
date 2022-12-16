a6ef2f700ffe512d41b43faddf3c6b4dd3e81e11
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-15 18:52:59 -0800

    dds

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,26 +8,25 @@
 
 int main(void)
 {
-	long int num, div, ans, maxFact;
+	long int num, div, maxFact;
 
 	num = 612852475143;
 
 	div = 2;
-	ans = 0;
-	while (n != 0)
+
+	while (num != 0)
 	{
-		if (n % div != 0)
+		if (num % div != 0)
 		{
 			div++;
 		}
 		else
 		{
-			maxFact = n;
-			n = n / div;
-			if (n == 1)
+			maxFact = num;
+			num = num / div;
+			if (num == 1)
 			{
-				printf("%d", maxFact);
-				ans = 1;
+				printf("%lu", maxFact);
 				break;
 			}
 		}
