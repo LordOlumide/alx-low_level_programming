47af6ff97c44cd99221eaf4add282c66996b66e3
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-19 15:10:46 -0800

    dssd

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by a newline
+ * @str: string to be printed
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\n'; i++)
+	{
+		_putschar(str[i]);
+	}
+}
