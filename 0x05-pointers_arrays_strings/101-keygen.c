9b8ae7daaf65e05f72cfa8783188ed978ce9786e
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-19 18:05:46 -0800

    assaassasasa

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <tima.h>
+
+/**
+ * main - Entry point
+ * Description: 'Generates random valid passwords'
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	return (o);
+}
