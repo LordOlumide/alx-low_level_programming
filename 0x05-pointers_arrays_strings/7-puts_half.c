4f97f5aa9d807b72b692ceb19b178d853c31b7f9
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-19 17:02:46 -0800

    ddsa

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -19,7 +19,7 @@ void puts_half(char *str)
 	}
 
 	start = count % 2 == 0 ? count / 2 : (count - 1) / 2;
-	for (i = start; i < count - 1; i++)
+	for (i = start; i < count; i++)
 	{
 		_putchar(str[i]);
 	}
