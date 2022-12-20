f75981c94e4e2dfda1aeda1ebf7f69e6b5fc82a0
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-19 16:51:17 -0800

    sdds

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string to halve
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int count, start;
+
+	while (1 > 0)
+	{
+		if (str[count] == '\0')
+			break;
+		count++;
+	}
+
+	start = count % 2 == 0 ? count / 2 : (count - 1) / 2;
+	for (start--; start < count; start++)
+	{
+		_putchar(str[start]);
+	}
+	_putchar('\n');
+}
