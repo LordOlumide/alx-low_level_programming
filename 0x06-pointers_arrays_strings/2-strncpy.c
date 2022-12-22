6ac5070c79f7e06e179931df7c44371fa174ebd4
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-21 16:52:36 -0800

    dsdsds

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * _strcpy - copies a string to another string
+ * _strncpy - copies a string to another string
  * @dest: destination string
  * @src: source string
  * @n: number of bytes to copy
  * Return: char *dest
  */
 
-char *_strcpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	int dest_len, i;
+	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len] = src[i];
+		dest[i] = src[i];
 	for (; i < n; i++)
 		dest[i] = '\0';
+	return dest;
 }
