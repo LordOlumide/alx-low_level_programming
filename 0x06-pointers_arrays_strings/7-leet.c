b08d3ba1a69dbe14b93a84f7138e8f3662ba9bfc
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-21 19:08:12 -0800

    sdfoinv

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * leet - encode into 1337
+ * @a: String to encode
+ * Return: char *
+ */
+
+char *leet(char *a)
+{
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		if (a[i] == 65 || a[i] == 97)
+			a[i] = 52;
+		else if (a[i] == 69 || a[i] == 101)
+			a[i] = 51;
+		else if (a[i] == 79 || a[i] == 111)
+			a[i] = 48;
+		else if (a[i] == 84 || a[i] == 116)
+			a[i] = 55;
+		else if (a[i] == 76 || a[i] == 108)
+			a[i] == 49;
+	}
+
+	return (a);
+}
