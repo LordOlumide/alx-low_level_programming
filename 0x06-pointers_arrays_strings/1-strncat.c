174edd820cc426f30efd41602a7fe0156d930550
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-21 16:11:32 -0800

    sdsd

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strncat - concats two strings
+ * @dest: first string
+ * @src: second string
+ * @n: max size of src used
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, src_len, sum_len, i;
+
+	dest_len = 0;
+	src_len = 0;
+	sum_len = 0
+
+	while (1)
+	{
+		if (dest[dest_len] == '\0')
+			break;
+		dest_len++;
+	}
+	while (1)
+	{
+		if (src[src_len] == '\0')
+			break;
+		src_len++;
+	}
+
+	sum_len = dest_len + src_len;
+	for (i = 0; i < src_len && i < n; i++)
+		dest[dest_len + i] = src[i];
+	dest[sum_len + 1] = '\n';
+
+	return (dest);
+}
