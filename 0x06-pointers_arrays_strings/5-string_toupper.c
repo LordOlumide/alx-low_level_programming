d9ff08e5216789d42253c1dae1bc936e7535b1b2
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-21 18:44:24 -0800

    sddsds

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,10 +10,10 @@ char *string_toupper(char *a)
 {
 	int i;
 
-	i = 0;
 	for (i = 0; a[i] != '\0'; i++)
 	{
 		if (a[i] >= 97 && a[i] <= 122)
 			a[i] = a[i] - 32;
 	}
+	return (a);
 }
---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,12 @@
+#incluse "main.h"
+
+/**
+ * cap_string - capitalizes each word
+ * @str: string to be capitalized
+ * Return: char *
+ */
+
+char *cap_string(char *a)
+{
+	return (a);
+}
