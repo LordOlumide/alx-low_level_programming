f3f01c2b368e0b58d0b364ebf46f4a6b0b39ccc9
Olubosede Olumide <olumideolubosede@gmail.com>
2022-12-21 16:26:09 -0800

    sdoi

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,11 +10,9 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len, src_len, sum_len, i;
+	int dest_len, i;
 
 	dest_len = 0;
-	src_len = 0;
-	sum_len = 0;
 
 	while (1)
 	{
@@ -22,17 +20,10 @@ char *_strncat(char *dest, char *src, int n)
 			break;
 		dest_len++;
 	}
-	while (1)
-	{
-		if (src[src_len] == '\0')
-			break;
-		src_len++;
-	}
 
-	sum_len = dest_len + src_len;
-	for (i = 0; i < src_len && i < n; i++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 		dest[dest_len + i] = src[i];
-	dest[sum_len + 1] = '\n';
+	dest[dest_len + 1] = '\0';
 
 	return (dest);
 }
