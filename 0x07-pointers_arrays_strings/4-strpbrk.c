17db30ab4cbf9b89fbabaabc1a2b59ac8e9583ad
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-03 13:46:31 -0800

    sdknwv

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strpbrk -  locates the first occurrence in the string s in accept
+ * @s: string to search
+ * @accept: String to find
+ *
+ * Return: char *
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, a, b;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		b = 0;
+		for (a = 0; accept[a] != '\0'; a++)
+		{
+			if (s[i] == accept[a])
+			{
+				b = 1;
+				break;
+			}
+		}
+		if (b == 1)
+			break;
+	}
+	return (s[i] == accept[a] ? &(s + i) : '\0');
+}
