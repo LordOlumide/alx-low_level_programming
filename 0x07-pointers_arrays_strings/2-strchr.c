cbab68d957836073204ea47131913dd83cb7a81c
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-03 12:57:31 -0800

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in a string
+ * @s: string to check
+ * @c: char to check for
+ *
+ * Return: char *
+ */
+
+char *_strchr(char *s, char c)
+{
+	int num, b;
+
+	num = 0;
+	while (s[num] != '\0')
+	{
+		num++;
+	}
+	for (b = 0; b < num; b++)
+	{
+		if (s[b] == c)
+			return (&(s[b]));
+	}
+	return (NULL);
+}
