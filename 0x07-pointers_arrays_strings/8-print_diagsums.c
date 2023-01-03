a371687ee3b903986f1f411bc9da1148899558b3
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-03 15:16:12 -0800

    sfnivjnw

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsums - suma the diagonals of a square matrix
+ * @a: square matrix
+ * @size: size of the matrix
+ *
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	unsigned int x, y, sum1 = 0, sum2 = 0;
+
+	for (x = 0, y = size - 1; x < (size * size); x += size + 1, y += size - 1)
+	{
+		sum1 += a[x];
+		sum2 += a[y];
+	}
+
+	printf('%i, %i\n', sum1, sum2);
+}
