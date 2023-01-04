3b4ba9377540676aedd041734f90542e3a476489
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-04 11:45:47 -0800

    dcds

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,7 +11,7 @@ int _sqrt_recursion(int n)
 	if (n < 0)
 		return -1;
 
-	return check_sqrt(n, 1);
+	return (check_sqrt(n, 1));
 }
 
 /**
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,6 @@ int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 int check_sqrt(int a, int b);
+int check_prime(int a, int b);
 
 #endif
