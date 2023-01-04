1665596661388d8e15a8f0701591f2e10dc257d7
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-04 11:27:37 -0800

    sdcce

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns natural sqaire root or -1
+ * @n: number to check
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return -1;
+
+	check_sqrt(n, 1);
+}
+
+/**
+ * check_sqrt - checks for sqrt
+ * @a: n
+ * @b: 1
+ *
+ * Return: int
+ */
+
+int check_sqrt(int a, int b)
+{
+	if (b * b == a)
+		return (b);
+	else if (b * b > a)
+		return (-1);
+	else
+		return (check_sqrt(a, b + 1));
+}
