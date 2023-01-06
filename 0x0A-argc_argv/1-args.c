57910a55761e23be5dd98593ba5f9c64cb522ebe
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-06 12:43:22 -0800

    cec

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Prints argc
+ * @argc: no.of args passed into main
+ * @argv: vector of string args passed into main
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+
+	printf("%d\n", argc);
+
+	Return (0);
+}
