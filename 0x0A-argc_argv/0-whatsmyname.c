a2b8e871bba223ead6599ae9ea5f81429c96e621
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-06 12:37:20 -0800

    ces

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,16 @@
 
 /**
  * main - Entry point
+ * @argc: no. of arguments passed to main
+ * @argv: Vector of string arguments passed to main
  * Description: THis program prints its name
  * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s", argv[0]);
+	return (0);
 }
