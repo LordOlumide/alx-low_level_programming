3f222db59355a036229cc6e2c7748280b7848d78
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-06 12:32:29 -0800

    ceacc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: THis program prints its name
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+}
