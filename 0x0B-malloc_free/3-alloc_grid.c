b84459c59c599c980926bc029b6c72524818f7a3
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-10 14:06:04 -0800

    cef

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,13 +18,20 @@ int **alloc_grid(int width, int height)
 
 	a = malloc(sizeof(int *) * height);
 	if (a == NULL)
+	{
+		free(a);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		a[i] = malloc(sizeof(int) * width);
 		if (a[i] == NULL)
 		{
+			for (j = i; j >= 0; j--)
+			{
+				free(a[j]);
+			}
 			return (NULL);
 		}
 	}
