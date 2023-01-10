aad17329e261200b6087e882e0d09cce70e382db
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-10 15:42:10 -0800

    cawsa

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - splits strings to words
+ * @str: string to split
+ *
+ * Return: char **
+ */
+
+char **strtow(char *str)
+{
+	int i, j, arr_len = 0;
+	char **value;
+
+	if (str == null || str == "")
+		return (NULL);
+
+	arr_len = 1;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			arr_len++;
+	}
+	arr_len++;
+
+	value = (char **)malloc(sizeof())
+}
