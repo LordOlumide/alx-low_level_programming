ccfc8e0d67f58a1fd698d26449e9e0dfdc685f57
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-10 14:00:23 -0800

    dsd

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a 2d array of ints
+ * @width: width
+ * @height: height
+ * Return: int **
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int **a;
+
+	if (width == NULL || height == NULL)
+		return (NULL);
+	if (width < 1 || height < 1)
+		return (NULL);
+
+	a = malloc(sizeof(int *) * height);
+	if (a == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		a[i] = malloc(sizeof(int) * width);
+		if (a[i] == NULL)
+		{
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < height; j++)
+		{
+			a[i][j] = 0;
+		}
+	}
+
+	return (a);
+}
