0387c4e67af0bcc0fbd934406250c97657797d01
Olubosede Olumide <olumideolubosede@gmail.com>
2023-01-18 14:58:23 -0800

    cascas

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -27,9 +27,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	doggo->name = _strcopy(doggo->name, name);
+	doggo->name = strcopy(doggo->name, name);
 	doggo->age = age;
-	doggo->owner = _strcopy(doggo->owner, owner);
+	doggo->owner = strcopy(doggo->owner, owner);
 
 	return (doggo);
 }
